822f95c881aff26a19b43919f090330f29b6050b
josearc <juanjose.arcani2309@gmail.com>
2021-11-30 18:36:19 -0400

    Readme added

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Babel Eslint Template 
+
+This project is meant to be used a a template for React applications usign Babel and ESlint. All wrapped up with Webpack
+
+### `npm install`
+
+Runs the app in development mode.\
+
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => (
-  <div>Titulo</div>
-)
+  <>
+    <div>Titulo</div>
+    <h1>This is a title</h1>
+    <p>Hello there, I am going to tel a sort story about how I met your mother</p>
+  </>
+);
 
-export default App
+export default App;
